0db575341a02661107d6db54db2b0704d35fd5c9
Francis Therien <frtherien@gmail.com>
2022-09-01 10:02:22 -0400

    Sort language IDs in numerical order

--- scripts/gen_lang_ids.nim
+++ scripts/gen_lang_ids.nim
@@ -4,40 +4,44 @@
 
 import std/strutils
 import std/strformat
+import std/algorithm
+
+const usbidsFileName = "./usb.ids"
+
+type LangIdEntry = object
+  id: int
+  fullName: string
+
+# For sorting
+func `<`(a, b: LangIdEntry): bool = a.id < b.id
 
 func sanitizeLangName(name: string): string =
   result = name
   result = result.replace(" ", "")
   result = result.replace(".", "")
 
-proc genLang(langId, sublangId: int, langName, sublangName: string) =
-    let fullLangId = langId or (sublangId shl 10)
-    let fullLangName = sanitizeLangName(
-      if langName == sublangName:
-        langName
-      else:
-        (langName & sublangName)
-    )
-    echo fmt"  {fullLangName} = {fullLangId:#06x}"
+func initLangEntry(langId, sublangId: int, langName, sublangName: string): LangIdEntry =
+  result.id = langId or (sublangId shl 10)
+  result.fullName = sanitizeLangName(
+    if langName == sublangName:
+      langName
+    else:
+      (langName & sublangName)
+  )
 
-let usbidsFileName = "./usb.ids"
+proc echoLangEntry(e: LangIdEntry) =
+  echo fmt"  {e.fullName} = {e.id:#06x}"
 
 var
   curlang = -1
   curlangName = ""
   sublangCount = 0
 
-echo """
-# Generated automatically by script gen_lang_ids.nim
-# Based on Linux-USB project usb.ids file
-"""
-
-echo "type LangId* {.size: 2, pure.} = enum"
-
+var langs: seq[LangIdEntry]
 for ln in usbidsFileName.lines:
   # Had a primary language on previous line without a sublang
   if curlang > 0 and sublangCount == 0 and not ln.startsWith("\t"):
-    genLang(curlang, 0x01, curlangName, "")
+    langs.add initLangEntry(curlang, 0x01, curlangName, "")
 
   if ln.startsWith("L "):
     let parts = ln.splitWhitespace(maxsplit=2)
@@ -52,4 +56,19 @@ for ln in usbidsFileName.lines:
       parts = ln.splitWhitespace(maxsplit=1)
       sublang = parts[0].parseHexInt
       sublangName = parts[1]
-    genLang(curlang, sublang, curlangName, sublangName)
+    langs.add initLangEntry(curlang, sublang, curlangName, sublangName)
+
+sort(langs)
+
+echo """
+# Generated automatically by script gen_lang_ids.nim
+# Based on Linux-USB project usb.ids file
+#
+# Sorry for the irregular ordering, but Nim requires enums to be sorted in
+# numerical order, and the USB spec encodes the sub-languages in higher bits.
+"""
+
+echo "type LangId* {.size: 2, pure.} = enum"
+
+for lang in langs:
+  echoLangEntry(lang)
