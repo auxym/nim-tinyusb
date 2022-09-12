fcaa6c04b99697d381b2bc360c182ffddc8baff6
Francis Therien <frtherien@gmail.com>
2022-09-11 21:16:30 -0400

    WIP on test for CDC descriptor

---
+++ tests/tusb_config.h
@@ -0,0 +1,12 @@
+#define CFG_TUD_ENABLED       1
+
+#ifndef CFG_TUD_ENDPOINT0_SIZE
+#define CFG_TUD_ENDPOINT0_SIZE    64
+#endif
+
+//------------- CLASS -------------//
+#define CFG_TUD_CDC              1
+#define CFG_TUD_MSC              0
+#define CFG_TUD_HID              1
+#define CFG_TUD_MIDI             0
+#define CFG_TUD_VENDOR           0
